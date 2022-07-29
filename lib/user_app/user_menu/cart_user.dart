19c82a7815fc7f8b4d9b7c141911aa3859e69f6d
Hammad Khan <hammad.khan0312@gmail.com>
2022-07-29 13:15:15 +0500

    user app progress

--- lib/user_app/user_menu/cart_user.dart
+++ lib/user_app/user_menu/cart_user.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:gigi_app/user_app/user_menu/cart_deals.dart';
 
 class Cart_user extends StatelessWidget {
   const Cart_user({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+        child: Scaffold(
+            body: Padding(
+                padding: const EdgeInsets.only(left: 24, right: 32, top: 17),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Container(
+                            width: 44,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                  // changes position of shadow
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(16),
+                              color: Colors.white,
+                            ),
+                            child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Image.asset(
+                                    'assets/images/arrow-left.png')),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 22),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Cafe Bistrovia',
+                                  style: const TextStyle(
+                                      fontFamily: 'Mulish',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xFF8E8EA9)),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  'Your cart',
+                                  style: const TextStyle(
+                                      fontFamily: 'Mulish',
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600,
+                                      color: Color(0xFF32324D)),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 24),
+                        child: Slidable(
+                          endActionPane:
+                              ActionPane(motion: ScrollMotion(), children: [
+                            SlidableAction(
+                              onPressed: (context) => {},
+                              backgroundColor: Colors.red,
+                              icon: Icons.delete,
+                            )
+                          ]),
+                          child: cart_deals(),
+                        ),
+                      )
+                    ]))));
   }
 }
