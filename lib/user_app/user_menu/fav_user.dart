adcec233bd91225932230a07976921e794992cd8
Hammad Khan <hammad.khan0312@gmail.com>
2022-07-30 19:18:16 +0500

    user app progress

--- lib/user_app/user_menu/fav_user.dart
+++ lib/user_app/user_menu/fav_user.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:gigi_app/user_app/user_menu/wishlist.dart';
+
+import '../../shared/custom_button.dart';
+import 'cart_deals.dart';
+import 'cart_user.dart';
+import 'order_status1.dart';
 
 class Fav_user extends StatelessWidget {
   const Fav_user({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+        child: Scaffold(
+            body: Padding(
+                padding: const EdgeInsets.only(left: 24, right: 32, top: 17),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Container(
+                            width: 44,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                  // changes position of shadow
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(16),
+                              color: Colors.white,
+                            ),
+                            child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Image.asset(
+                                    'assets/images/arrow-left.png')),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 22),
+                            child: Text(
+                              'Your Wishlist',
+                              style: const TextStyle(
+                                  fontFamily: 'Mulish',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFF32324D)),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 24),
+                        child: Wishlist(),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      CustomButton(
+                          text: 'Add to Cart ➔',
+                          onPressed: () {
+                            Navigator.of(context).push(
+                                MaterialPageRoute(builder: (_) => Cart_user()));
+                          })
+                    ]))));
   }
 }
---
+++ lib/user_app/user_menu/wishlist.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../../constant/size_constants.dart';
+
+class Wishlist extends StatelessWidget {
+  const Wishlist({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Container(
+      width: SizeConfig.screenWidth,
+      height: 120,
+      decoration: BoxDecoration(
+          color: Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      child: Row(
+        children: [
+          Image.asset(
+            'assets/images/cart_deal.png',
+            height: 119,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 21),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      'Avocado Chicken \nSalad (Medium)',
+                      style: TextStyle(
+                          fontFamily: 'Mulish',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF32324D)),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(top: 9),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/menu_location.png',
+                          width: 8,
+                          height: 8,
+                        ),
+                        Text(
+                          'Cafe Bistrovia - Baku, Azerbaijan',
+                          style: TextStyle(
+                              fontFamily: 'Mulish',
+                              fontSize: 10,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF848484)),
+                        ),
+                      ],
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/images/rating.png',
+                            width: 6, height: 6),
+                        Text(
+                          '4.8',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 7,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF5F5F5F)),
+                        ),
+                        Text(
+                          '(30 reviews)',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 4,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF5F5F5F)),
+                        ),
+                      ],
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(top: 2.83),
+                    child: Row(
+                      children: [
+                        Text(
+                          '\$',
+                          style: TextStyle(
+                              fontFamily: 'Mulish',
+                              fontSize: 9,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFFFF6767)),
+                        ),
+                        Text(
+                          '10.40',
+                          style: TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              fontFamily: 'Mulish',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFFFF6767)),
+                        ),
+                        Text(
+                          '\$',
+                          style: TextStyle(
+                              fontFamily: 'Mulish',
+                              fontSize: 9,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF0D9BFF)),
+                        ),
+                        Text(
+                          '8.40',
+                          style: TextStyle(
+                              fontFamily: 'Mulish',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF0D9BFF)),
+                        ),
+                        Container(
+                          width: 28,
+                          height: 11,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3))),
+                          child: Center(
+                            child: Text(
+                              '20% OFF',
+                              style: TextStyle(
+                                  fontSize: 5,
+                                  fontFamily: 'Mulish',
+                                  fontWeight: FontWeight.w900,
+                                  color: Color(0xFF0D9BFF)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
